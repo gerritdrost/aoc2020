1725b6d645d60cbc8112425bc4f9f6f0deffe6df
Gerrit Drost <gdrost@vcn.nl>
2020-12-03 07:05:38 +0100

    Day 3 part 2 + code cleanup

--- bin/day3_1.php
+++ bin/day3_1.php
@@ -1,34 +1,23 @@
 <?php
 
 use DusanKasan\Knapsack\Collection;
+use GerritDrost\AoC2020\Arrays;
 use GerritDrost\AoC2020\Files;
-use GerritDrost\AoC2020\Generators;
 
 require __DIR__ . '/../bootstrap.php';
 
-$solver = function (): int {
-    $inputHandle = Files::openForReading(PROJECT_ROOT_DIR . '/res/day/3/input');
+$solver = function (): int {$inputHandle = Files::openForReading(PROJECT_ROOT_DIR . '/res/day/3/input');
+    $lines       = Arrays::linesFromHandle($inputHandle);
+    $width       = strlen($lines[0]);
 
-    $lineGenerator = Generators::linesFromHandle($inputHandle);
-
-    // Takes line as input, returns the character from this line on the toboggan path
-    $mapToPathChar = function (string $line, int $y) {
-        // no mb_ since ASCII
-        $width = strlen($line);
-
-        // Determine x-coordinate.
-        $x = ($y * 3) % $width;
-
-        return substr($line, $x, 1);
-    };
-
+    $dx = 3;
     $treeCount = Collection
-        ::from($lineGenerator)
-        ->map($mapToPathChar)
+        ::from($lines)
+        ->map(fn($line, $y) => substr($line, ($y * $dx) % $width, 1))
         ->filter(fn (string $char) => $char === '#')
         ->size();
 
-    echo "\nTrees: $treeCount\n";
+    echo "Trees: $treeCount\n";
 
     return 0;
 };
---
+++ src/Arrays.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GerritDrost\AoC2020;
+
+use Generator;
+
+class Arrays
+{
+    /**
+     * @param resource $handle
+     *
+     * @return string[]
+     */
+    public static function linesFromHandle($handle): array
+    {
+        return iterator_to_array(Generators::linesFromHandle($handle));
+    }
+
+    /**
+     * @param resource $handle
+     *
+     * @return int[]
+     */
+    public static function intLinesFromHandle($handle): array
+    {
+        return iterator_to_array(Generators::intLinesFromHandle($handle));
+    }
+}
