1e60ce934825d8d4603a306827b765cfb653578f
Gerrit Drost <gdrost@vcn.nl>
2020-12-07 10:33:52 +0100

    If it's named $stack, it's gotta be pushed upon

--- src/Day7/Solver1.php
+++ src/Day7/Solver1.php
@@ -13,15 +13,15 @@ class Solver1 implements Solver
         $lines = Arrays::linesFromHandle($inputHandle);
 
         $result = [];
-        $stack = ['shiny gold'];
+        $stack  = ['shiny gold'];
         while (!empty($stack)) {
-            $description = array_pop($stack);
+            $description     = array_pop($stack);
             $validContainers = $this->validContainers($lines, $description);
 
             $newContainers = array_diff($validContainers, $result);
             foreach ($newContainers as $newContainer) {
+                array_push($stack, $newContainer);
                 $result[] = $newContainer;
-                $stack[] = $newContainer;
             }
         }
 
@@ -31,9 +31,9 @@ class Solver1 implements Solver
     private function validContainers(array $rules, string $description): array
     {
         return Collection::from($rules)
-            ->map(fn($l) => explode(' bags contain ', $l, 2))
-            ->filter(fn($a) => str_contains($a[1], $description))
-            ->map(fn($a) => $a[0])
+            ->map(fn ($l) => explode(' bags contain ', $l, 2))
+            ->filter(fn ($a) => str_contains($a[1], $description))
+            ->map(fn ($a) => $a[0])
             ->toArray();
     }
 }
